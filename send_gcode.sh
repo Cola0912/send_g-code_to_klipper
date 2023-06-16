40f187b59a439787442571f3e60416ecb0b83b4c
shusei Akiya <0912cocacola@gmail.com>
2023-06-16 14:57:49 +0900

    update

--- send_gcode.sh
+++ send_gcode.sh
@@ -3,11 +3,7 @@
 # api_keys.confからAPIキーを取得する関数
 get_api_key() {
     PRINTER=$1
-    API_KEY=$(grep "^$PRINTER" api_keys.conf | cut -d '=' -f2)
-    if [ -z "$API_KEY" ]; then
-        echo "Error: No API key found for printer $PRINTER"
-        exit 1
-    fi
+    API_KEY=$(grep "^$PRINTER" /usr/local/bin/api_keys.conf | cut -d '=' -f2)
     echo $API_KEY
 }
 
@@ -16,21 +12,28 @@ add_api_key() {
     PRINTER=$1
     API_KEY=$2
     # api_keys.confが存在しない場合は作成
-    if [ ! -f api_keys.conf ]; then
-        touch api_keys.conf
+    if [ ! -f /usr/local/bin/api_keys.conf ]; then
+        touch /usr/local/bin/api_keys.conf
     fi
-    echo "$PRINTER=$API_KEY" >> api_keys.conf
+    echo "$PRINTER=$API_KEY" >> /usr/local/bin/api_keys.conf
 }
 
 # api_keys.confからAPIキーを削除する関数
 del_api_key() {
     PRINTER=$1
-    sed -i "/^$PRINTER/d" api_keys.conf
+    sed -i "/^$PRINTER/d" /usr/local/bin/api_keys.conf
 }
 
 # api_keys.confから全てのプリンター名とAPIキーを表示する関数
 list_api_keys() {
-    cat api_keys.conf
+    cat /usr/local/bin/api_keys.conf
+}
+
+# IPアドレスを指定してG-codeを送信する関数
+send_gcode_to_ip() {
+    IP_ADDRESS=$1
+    GCODE=$2
+    curl -H "Content-Type: application/json" -X POST -d "{\"script\":\"$GCODE\"}" http://$IP_ADDRESS:7125/printer/gcode/script
 }
 
 # コマンドライン引数に基づいて処理を分岐
@@ -44,10 +47,18 @@ case $1 in
     "list")
         list_api_keys
         ;;
+    "ip")
+        send_gcode_to_ip $2 $3
+        ;;
     *)
         PRINTER=$1
         GCODE=$2
         API_KEY=$(get_api_key $PRINTER)
+        if [ -z "$API_KEY" ]; then
+            echo "Error: No API key found for printer $PRINTER"
+            exit 1
+        fi
         curl -H "X-Api-Key:$API_KEY" -H "Content-Type: application/json" -X POST -d "{\"script\":\"$GCODE\"}" http://localhost:7125/printer/gcode/script
         ;;
 esac
+
